48b2e814d78b568d13248c2bf46651772a23feb5
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2023-08-29 10:43:42 -0600

    Tenemos dos botontes, cuando presionamos el botón el motor se movera, cuando presionamos el otro botón el motor va a parar. Solo es necesario presionar una vez

--- PrenderApagarMotor/PrenderApagarMotor.ino
+++ PrenderApagarMotor/PrenderApagarMotor.ino
@@ -1,30 +1,45 @@
+//Variables
 int estado = 0;
+int prendido = 0;
+int apagado = 0;
+
 //MOTOR
 int motor_dir_1 = 6;
 int motor_dir_2 = 5;
 
 //Botones
-int boton = 2;
+int boton_encendido = 2;
+int boton_apagado = 3;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(boton, INPUT);
+  pinMode(boton_encendido, INPUT);
+  pinMode(boton_apagado, INPUT);
   
   pinMode(motor_dir_1, OUTPUT);
   pinMode(motor_dir_2, OUTPUT);
 }
 
 void loop() {
-  estado = digitalRead(boton);
+  prendido = digitalRead(boton_encendido);
+  apagado = digitalRead(boton_apagado);
 
-  if(estado == 1){
+  if(prendido and apagado == 0){
+    estado = 1;
+  }
+  else if (prendido == 0 and apagado == 1){
+    estado = 0;
+  }
+
+  if(estado){
     digitalWrite(motor_dir_1, HIGH); 
     digitalWrite(motor_dir_2, LOW); 
     Serial.println("Prendido");
   }
-  else{
+  else {
     digitalWrite(motor_dir_1, LOW); 
     digitalWrite(motor_dir_2, LOW); 
   }
+  
 
 }
