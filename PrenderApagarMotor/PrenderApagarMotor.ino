7fc94145e6078207247aa4ef8cf6aead1b637e73
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2023-08-29 12:57:07 -0600

    Adelante para atras 2 motores

--- PrenderApagarMotor/PrenderApagarMotor.ino
+++ PrenderApagarMotor/PrenderApagarMotor.ino
@@ -1,45 +1,53 @@
 //Variables
-int estado = 0;
-int prendido = 0;
-int apagado = 0;
+int adelante = 0;
+int atras = 0;
 
 //MOTOR
 int motor_dir_1 = 6;
 int motor_dir_2 = 5;
 
+int motor_2_dir_1 = 9;
+int motor_2_dir_2 = 10;
+
 //Botones
-int boton_encendido = 2;
-int boton_apagado = 3;
+int adelante_btn = 2;
+int atras_btn = 3;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(boton_encendido, INPUT);
-  pinMode(boton_apagado, INPUT);
+  pinMode(adelante_btn, INPUT);
+  pinMode(adelante_btn, INPUT);
   
   pinMode(motor_dir_1, OUTPUT);
   pinMode(motor_dir_2, OUTPUT);
+  pinMode(motor_2_dir_1, OUTPUT);
+  pinMode(motor_2_dir_2, OUTPUT);
 }
 
 void loop() {
-  prendido = digitalRead(boton_encendido);
-  apagado = digitalRead(boton_apagado);
+  adelante = digitalRead(adelante_btn);
+  atras = digitalRead(atras_btn);
 
-  if(prendido and apagado == 0){
-    estado = 1;
-  }
-  else if (prendido == 0 and apagado == 1){
-    estado = 0;
-  }
-
-  if(estado){
+  if(adelante and atras == 0){
     digitalWrite(motor_dir_1, HIGH); 
     digitalWrite(motor_dir_2, LOW); 
-    Serial.println("Prendido");
+    digitalWrite(motor_2_dir_1, HIGH); 
+    digitalWrite(motor_2_dir_2, LOW); 
+    Serial.println("Adelante");
   }
-  else {
+  else if (adelante == 0 and atras == 1){
+    digitalWrite(motor_dir_1, LOW); 
+    digitalWrite(motor_dir_2, HIGH); 
+    digitalWrite(motor_2_dir_1, LOW); 
+    digitalWrite(motor_2_dir_2, HIGH); 
+    Serial.println("Atras");
+  }
+  else{
     digitalWrite(motor_dir_1, LOW); 
     digitalWrite(motor_dir_2, LOW); 
+    digitalWrite(motor_2_dir_1, LOW); 
+    digitalWrite(motor_2_dir_2, LOW); 
+    Serial.println("Apagado");
   }
   
-
 }
